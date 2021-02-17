6e104468a027fa841498b9615ed3687760b088aa
gregalantiu2 <tiugreg2@gmail.com>
2021-02-16 23:30:28 -0500

    Progress on algo

--- StonkProgram.cs
+++ StonkProgram.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,12 +26,43 @@ namespace StockTrading.Libraries
 
         public async Task RunDayTradingProgram(List<string> symbols)
         {
-            //Dictionary<string, decimal> transactions = new Dictionary<string, decimal>();
+            Dictionary<string, string> transactions = new Dictionary<string, string>();
 
-            //transactions = await GetStonkPriceChange(symbols);
+            Dictionary<string, decimal> priceChanges = new Dictionary<string, decimal>();
 
-            //foreach(var )
+            priceChanges = await GetStonkPriceChange(symbols);
 
+            foreach(var stonk in priceChanges)
+            {
+                if(stonk.Value < -3)
+                {
+                    try
+                    {
+                        var order = await alpacaTradingClient.PostOrderAsync(TrailingStopOrder.Buy(stonk.Key, 25, TrailOffset.InDollars(0.80M)));
+                        transactions.Add("Buy", "Bought " + stonk.Key + " at " + stonk.Value.ToString());
+                    }
+                    catch(HttpRequestException e)
+                    {
+                        transactions.Add("Error", e.Message);
+                    }
+                }
+                else if(stonk.Value > 1)
+                {
+                    try
+                    {
+                        var order = await alpacaTradingClient.PostOrderAsync(TrailingStopOrder.Sell(stonk.Key, 25, TrailOffset.InDollars(0.80M)));
+                        transactions.Add("Trailing Sell Posted", "Trailing stop order posted for " + order.Symbol + "trailing at "  + order.TrailOffsetInDollars.ToString());
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        transactions.Add("Error", e.Message);
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
 
         }
         public async Task<Tuple<bool,DateTime>> MarketTimesGet()
@@ -46,7 +78,7 @@ namespace StockTrading.Libraries
             foreach(var symbol in symbols)
             {
                 var bars = await alpacaDataClient.GetBarSetAsync(new BarSetRequest(symbol, TimeFrame.FifteenMinutes) {Limit = 1 });
-
+                
                 var startPrice = bars[symbol].First().Open;
                 var endPrice = bars[symbol].Last().Close;
 
