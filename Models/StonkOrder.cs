60c39598e083daa0da40c34153732ce722dfafaf
gregalantiu2 <tiugreg2@gmail.com>
2021-02-27 12:01:33 -0500

    Added models folder

---
+++ Models/StonkOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockTrading.Libraries.Models
+{
+    public class StonkOrder
+    {
+        public List<string> Symbols { get; set; }
+        public int Quantity { get; set; }
+        public decimal TrailAmount { get; set; }
+    }
+}
