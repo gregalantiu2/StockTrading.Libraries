b9b5eddf70749e7aa16af72f8bf7f29c059f01e7
gregalantiu2 <tiugreg2@gmail.com>
2021-02-28 12:52:01 -0500

    Added test project inside library directory

---
+++ StockTrading.Test/Keys/AlpacaAPI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockTrading.Libraries.Keys
+{
+    public static partial class AlpacaAPIKeys
+    {
+        public static readonly string ApiKey = "";
+        public static readonly string SecretKey = "";
+    }
+}
